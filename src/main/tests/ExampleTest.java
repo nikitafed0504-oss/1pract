2d8198d12c3707dc338496aef6e5d9b18f8eaf9f
никита <.mail.com>
2025-12-12 13:34:17 +0300

    feat: add test directory with ExampleTest

---
+++ src/main/tests/ExampleTest.java
@@ -0,0 +1,11 @@
+﻿package ru.mtuci.coursemanagement;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+    @Test
+    public void testExample() {
+        assertTrue(true, "Test should pass");
+    }
+}
