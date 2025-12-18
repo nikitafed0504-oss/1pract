58416bcc20c2870a975358b11704ac48018cb687
никита <.mail.com>
2025-12-18 10:46:31 +0300

    feat: add test directory with correct structure

---
+++ src/test/java/ru/mtuci/coursemanagement/CourseManagementApplicationTests.java
@@ -0,0 +1,14 @@
+﻿package ru.mtuci.coursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class CourseManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true, "Context should load successfully");
+    }
+}
